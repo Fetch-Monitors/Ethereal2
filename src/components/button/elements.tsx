46754a072bbada0620795e99d99c4bdcc706fb86
Maximilian Hollis <hollismaximilian@gmail.com>
2022-08-18 21:05:02 -0400

    build enhancements

--- src/components/button/elements.tsx
+++ src/components/button/elements.tsx
@@ -1,137 +1,137 @@
 import styled from 'styled-components'
 
 export const Button = styled.button<{
-  variant: 'solid' | 'outlined' | 'text'
-  secondary: boolean
-  disabled: boolean
-  $loading?: boolean
+	variant: 'solid' | 'outlined' | 'text'
+	secondary: boolean
+	disabled: boolean
+	$loading?: boolean
 }>`
-  border: 0;
-  border-radius: ${(props) => props.theme.borderRadius};
-  width: 100%;
-  box-sizing: border-box;
-  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
-  display: inline-block;
-  font-size: 14px;
-  font-weight: 550;
-  line-height: 1;
-  outline: none;
-  padding: 1rem 2rem;
-  text-align: center;
-  text-decoration: none;
-  text-transform: uppercase;
-  user-select: none;
-  white-space: nowrap;
-  box-sizing: border-box;
-  -webkit-tap-highlight-color: transparent;
-  background-color: ${(props) =>
-    props.variant === 'solid'
-      ? props.secondary
-        ? props.theme.secondary
-        : props.theme.primary
-      : 'transparent'};
-  ${(props) =>
-    props.variant === 'outlined'
-      ? `
+	border: 0;
+	border-radius: ${(props) => props.theme.borderRadius};
+	width: 100%;
+	box-sizing: border-box;
+	cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
+	display: inline-block;
+	font-size: 14px;
+	font-weight: 550;
+	line-height: 1;
+	outline: none;
+	padding: 1rem 2rem;
+	text-align: center;
+	text-decoration: none;
+	text-transform: uppercase;
+	user-select: none;
+	white-space: nowrap;
+	box-sizing: border-box;
+	-webkit-tap-highlight-color: transparent;
+	background-color: ${(props) =>
+		props.variant === 'solid'
+			? props.secondary
+				? props.theme.secondary
+				: props.theme.primary
+			: 'transparent'};
+	${(props) =>
+		props.variant === 'outlined'
+			? `
       border: 2px solid ${
-        props.secondary ? props.theme.secondary : props.theme.primary
-      };
+				props.secondary ? props.theme.secondary : props.theme.primary
+			};
     `
-      : `
+			: `
       padding: calc(1rem + 2px) calc(2rem + 2px);
     `}
 
-  ${(props) =>
-    (props.disabled || props.$loading) &&
-    `
+	${(props) =>
+		(props.disabled || props.$loading) &&
+		`
       filter: brightness(50%);
     `}
 
     color: ${(props) =>
-    props.variant === 'solid'
-      ? '#ffffff'
-      : props.secondary
-      ? props.theme.secondary
-      : props.theme.primary};
+		props.variant === 'solid'
+			? '#ffffff'
+			: props.secondary
+			? props.theme.secondary
+			: props.theme.primary};
 
-  position: relative;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-  ${({ variant }) =>
-    variant === 'text' &&
-    `
+	position: relative;
+	box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+	${({ variant }) =>
+		variant === 'text' &&
+		`
     box-shadow: none;
   `}
-  -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
-  transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
-  -webkit-font-smoothing: subpixel-antialiased;
-  backface-visibility: hidden;
+	-webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+	transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+	-webkit-font-smoothing: subpixel-antialiased;
+	backface-visibility: hidden;
 
-  ::after {
-    content: '';
-    position: absolute;
-    border-radius: ${(props) => props.theme.borderRadius};
-    z-index: -1;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
-    ${({ variant, theme, secondary }) =>
-      variant === 'text' &&
-      `
+	::after {
+		content: '';
+		position: absolute;
+		border-radius: ${(props) => props.theme.borderRadius};
+		z-index: -1;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+		${({ variant, theme, secondary }) =>
+			variant === 'text' &&
+			`
     box-shadow: none;
     background: ${secondary ? theme.secondary + 30 : theme.primary + 30};
   `}
-    opacity: 0;
-    -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
-    transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
-  }
+		opacity: 0;
+		-webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+		transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+	}
 
-  :active:hover {
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
-    ${({ variant }) =>
-      variant === 'text' &&
-      `
+	:active:hover {
+		box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+		${({ variant }) =>
+			variant === 'text' &&
+			`
     box-shadow: none;
   `}
-  }
+	}
 
-  :hover::after {
-    opacity: 1;
-  }
-  .rippleBox {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    top: 0;
-    left: 0;
-    .ripple {
-      position: absolute;
+	:hover::after {
+		opacity: 1;
+	}
+	.rippleBox {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		top: 0;
+		left: 0;
+		.ripple {
+			position: absolute;
 
-      border-radius: 50%;
-      transform: scale(0);
-      animation: ripple 600ms linear;
-      background-color: ${(props) =>
-        props.variant === 'solid'
-          ? 'inherit'
-          : `${
-              props.secondary ? props.theme.secondary : props.theme.primary
-            }44`};
-    }
+			border-radius: 50%;
+			transform: scale(0);
+			animation: ripple 600ms linear;
+			background-color: ${(props) =>
+				props.variant === 'solid'
+					? 'inherit'
+					: `${
+							props.secondary ? props.theme.secondary : props.theme.primary
+					  }44`};
+		}
 
-    @keyframes ripple {
-      to {
-        transform: scale(4);
-        opacity: 0;
-      }
-    }
-  }
+		@keyframes ripple {
+			to {
+				transform: scale(4);
+				opacity: 0;
+			}
+		}
+	}
 `
 
 export const Loader = styled.div`
-  position: absolute;
-  left: 15px;
-  top: 25%;
-  transform: translateY(-50%) scale(0.5);
+	position: absolute;
+	left: 15px;
+	top: 25%;
+	transform: translateY(-50%) scale(0.5);
 `
--- src/components/button/loader.tsx
+++ src/components/button/loader.tsx
@@ -1,90 +1,92 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from 'react'
 
 import { Button } from './elements'
 
 export default ({
-  children,
-  variant,
-  disabled,
-  secondary,
-  onClick,
-  type,
+	children,
+	variant,
+	disabled,
+	secondary,
+	onClick,
+	type,
 }: {
-  children: React.ReactNode
-  variant?: 'solid' | 'outlined' | 'text'
-  disabled?: boolean
-  secondary?: boolean
-  onClick: VoidFunction
-  type?: 'button' | 'submit' | 'reset' | undefined
+	children: React.ReactNode
+	variant?: 'solid' | 'outlined' | 'text'
+	disabled?: boolean
+	secondary?: boolean
+	onClick: VoidFunction
+	type?: 'button' | 'submit' | 'reset' | undefined
 }) => {
-  const [loading, setLoading] = useState(false)
-  const [err, setErr] = useState('')
+	const [loading, setLoading] = useState(false)
+	const [err, setErr] = useState('')
 
-  const click = async () => {
-    try {
-      if (loading) {
-        console.log('button is already loading')
-      } else {
-        setLoading(true)
-        await onClick()
-        setLoading(false)
-        setErr('')
-      }
-    } catch (e) {
-      setErr(e + '')
-      setLoading(false)
-    }
-  }
+	const click = async () => {
+		try {
+			if (loading) {
+				console.log('button is already loading')
+			} else {
+				setLoading(true)
+				await onClick()
+				setLoading(false)
+				setErr('')
+			}
+		} catch (e) {
+			setErr(`${e}`)
+			setLoading(false)
+		}
+	}
 
-  function createRipple(event: {
-    currentTarget: any
-    clientX: number
-    clientY: number
-  }) {
-    setLoading(!loading)
-    const button = event.currentTarget
-    const circle = document.createElement('span')
-    const diameter = Math.max(button.clientWidth, button.clientHeight)
-    const radius = diameter / 2
-    const offsetTop = button.getBoundingClientRect().top
-    const offsetLeft = button.getBoundingClientRect().left
-    circle.style.width = circle.style.height = `${diameter}px`
-    circle.style.left = `${event.clientX - offsetLeft - radius}px`
-    circle.style.top = `${event.clientY - offsetTop - radius}px`
-    circle.classList.add('ripple')
+	function createRipple(event: {
+		currentTarget: any
+		clientX: number
+		clientY: number
+	}) {
+		setLoading(!loading)
+		const button = event.currentTarget
+		const circle = document.createElement('span')
+		const diameter = Math.max(button.clientWidth, button.clientHeight)
+		const radius = diameter / 2
+		const offsetTop = button.getBoundingClientRect().top
+		const offsetLeft = button.getBoundingClientRect().left
+		circle.style.height = `${diameter}px`
+		circle.style.width = circle.style.height
+		circle.style.left = `${event.clientX - offsetLeft - radius}px`
+		circle.style.top = `${event.clientY - offsetTop - radius}px`
+		circle.classList.add('ripple')
 
-    const ripple = button.getElementsByClassName('ripple')[0]
+		const ripple = button.getElementsByClassName('ripple')[0]
 
-    if (ripple) {
-      ripple.remove()
-    }
-    if (!disabled) {
-      button.appendChild(circle)
-    }
-  }
-  return (
-    <React.Fragment>
-      <Button
-        variant={variant || 'solid'}
-        disabled={!!disabled}
-        secondary={!!secondary}
-        onClick={click}
-        style={
-          loading
-            ? { cursor: 'wait', filter: 'brightness(90%)' }
-            : { cursor: 'pointer' }
-        }
-        type={type}
-        title={err}
-      >
-        {children}
-        {/* {loading && (
+		if (ripple) {
+			ripple.remove()
+		}
+
+		if (!disabled) {
+			button.appendChild(circle)
+		}
+	}
+
+	return (
+		<Button
+			variant={variant || 'solid'}
+			disabled={!!disabled}
+			secondary={!!secondary}
+			style={
+				loading
+					? { cursor: 'wait', filter: 'brightness(90%)' }
+					: { cursor: 'pointer' }
+			}
+			type={type}
+			title={err}
+			onClick={click}
+		>
+			{children}
+			{/* {loading && (
           <Loader>
             <FadeLoader height={10} margin={1} width={6} color='white' />
           </Loader>
         )} */}
-        <span className="rippleBox" onClick={createRipple} />
-      </Button>
-    </React.Fragment>
-  )
+			<span className="rippleBox" onClick={(e) => createRipple(e)} />
+		</Button>
+	)
 }
